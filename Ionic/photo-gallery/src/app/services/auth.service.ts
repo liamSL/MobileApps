36b1878504624fb561ede5ec0844a3b970e6f97e
LiamSL <liamleung@outlook.com>
2021-10-28 13:37:12 -0400

    lab3 b fin

--- Ionic/photo-gallery/src/app/services/auth.service.ts
+++ Ionic/photo-gallery/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/auth';
+import { DatabaseService } from './database.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class AuthService {
   userData: any = {
     email: ""
   }
-  constructor() {}
+  constructor(private database: DatabaseService) {}
 
   loggedIn = false;
     
@@ -33,6 +34,7 @@ export class AuthService {
       firebase.signInWithPopup(firebase.getAuth(), provider).then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       // result.credential.accessToken;
+      this.database.writeLoginToDatabase(result.user.email);
       this.loggedIn = true;
       this.userData.email = result.user.email;
       }).catch((error) => {
--- Ionic/photo-gallery/src/app/tab3/tab3.page.ts
+++ Ionic/photo-gallery/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public database: DatabaseService) {}
 
 }
